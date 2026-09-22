972e1e2bd638986ea6ba6e9c44762d370f28c670
marvenpiadri <marvenpiadri@gmail.com>
2026-09-22 21:43:53 +0100

    build: configure Angular HttpClient for data services

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -4,6 +4,7 @@ import {
   provideAppInitializer,
   provideBrowserGlobalErrorListeners,
 } from '@angular/core';
+import { provideHttpClient } from '@angular/common/http';
 import { provideRouter } from '@angular/router';
 import { routes } from './app.routes';
 import { SeoService } from './core/services/seo.service';
@@ -11,6 +12,7 @@ import { SeoService } from './core/services/seo.service';
 export const appConfig: ApplicationConfig = {
   providers: [
     provideBrowserGlobalErrorListeners(),
+    provideHttpClient(),
     provideRouter(routes),
     provideAppInitializer(() => {
       inject(SeoService);

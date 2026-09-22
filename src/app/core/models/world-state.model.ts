2a05e7a17f5c0c78d38220f0460c01a28908f0cb
marvenpiadri <marvenpiadri@gmail.com>
2026-09-22 21:43:39 +0100

    build: add core world-state and data services

---
+++ src/app/core/models/world-state.model.ts
@@ -0,0 +1,22 @@
+import { CelestialState } from './celestial.model';
+import { GeoLocation } from './location.model';
+import { WeatherData } from './weather.model';
+
+export interface WorldState {
+  instant: Date;
+  location: GeoLocation;
+  timezone: string;
+  localTime: string;
+  localDate: string;
+  utcTime: string;
+  utcOffsetMinutes: number;
+  celestial: CelestialState;
+  weather: WeatherData;
+  sunPhase: CelestialState['twilightState'];
+  isDaylight: boolean;
+  isAstronomicalNight: boolean;
+  timeOfDayFraction: number;
+  solarIntensity: number;
+  lunarIllumination: number;
+  skyVisibility: number;
+}

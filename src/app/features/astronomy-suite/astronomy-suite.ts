73c53e054d23ad74142eb28d310babfab3030939
marvenpiadri <marvenpiadri@gmail.com>
2026-09-25 11:52:52 +0100

    Import Angular effect for astronomy SEO state

--- src/app/features/astronomy-suite/astronomy-suite.ts
+++ src/app/features/astronomy-suite/astronomy-suite.ts
@@ -1,4 +1,4 @@
-import { ChangeDetectionStrategy, Component, computed, inject, signal, OnInit } from '@angular/core';
+import { ChangeDetectionStrategy, Component, computed, effect, inject, signal, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
 import { ActivatedRoute, Router, RouterModule } from '@angular/router';
